df8b89476fc16c1b94b2e5500cfdce6ee739f46f
ilkinellada <ilkin@gmail.com>
2024-06-03 01:29:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Dictionary representing the Morse code chart
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.',
+    ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-',
+    '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+def string_to_morse_code(text):
+    morse_code = ''
+    for char in text.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code += MORSE_CODE_DICT[char] + ' '
+        else:
+            morse_code += ' '  # If character is not in the dictionary, just add a space
+    return morse_code.strip()
+
+if __name__ == "__main__":
+    # Input string
+    input_string = input("Enter a string to convert to Morse Code: ")
+    # Convert the string to Morse code
+    morse_code = string_to_morse_code(input_string)
+    # Print the Morse code
+    print(f"Morse Code: {morse_code}")
